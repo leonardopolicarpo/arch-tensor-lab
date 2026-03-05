a0ade53383fad8c6c7514b08ddd7ea03f6cfbae2
Leonardo Policarpo <leo.dumont@outlook.com>
2026-03-05 16:16:02 -0300

    feat: initialize tokenizer vocabulary and character-to-index mappings

---
+++ src/data/tokenizer.py
@@ -0,0 +1,24 @@
+import os
+
+class Tokenizer:
+  def __init__(self, raw_text_path: str) -> None:
+    if not os.path.exists(raw_text_path):
+      raise FileNotFoundError(f"File not found: {raw_text_path}")
+    
+    with open(raw_text_path, 'r', encoding='utf-8') as file:
+      self.text = file.read()
+
+    self.characters = sorted(list(set(self.text)))
+    self.vocabulary_size = len(self.characters)
+
+    self.string_to_index = { character:index for index,character in enumerate(self.characters) }
+    self.index_to_string = { index:character for index,character in enumerate(self.characters) }
+
+  def encode(self, string: str) -> list[int]:
+    pass
+
+  def decode(self, list_of_integers: list[int]) -> str:
+    pass
+
+  def save_data(self, output_path: str) -> None:
+    pass
